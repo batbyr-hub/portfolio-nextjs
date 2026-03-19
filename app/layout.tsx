dfdb20d0482e69aa928b6ed3a4e71bdd083a1fb0
batbyr-hub <batbayar.alt@gmail.com>
2026-03-19 14:06:06 -0600

    Fixed render Analytics and Speed Insights.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import type { Metadata } from "next";
 import { Cormorant_Garamond, Space_Grotesk } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import { Analytics } from "@vercel/analytics/next"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const displayFont = Cormorant_Garamond({
   subsets: ["latin"],
@@ -65,6 +65,8 @@ export default function RootLayout({
           </main>
           <Footer />
         </div>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
